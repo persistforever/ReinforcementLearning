f975235fdafd7da8814ce3ff52e857d342c5001a
ronniecao <ronniecao@DESKTOP-1CFBSNR>
2017-05-11 22:30:06 +0800

    20170511

--- gambler/mdp.py
+++ gambler/mdp.py
@@ -21,9 +21,11 @@ class MDP:
         
     def value_iteration(self):
         n_iter = 0
+        value_functions = []
         while True:
             n_iter += 1
             last_value_function = copy.deepcopy(self.value_function)
+            value_functions.append(last_value_function)
             print n_iter, list(enumerate(last_value_function))
             new_value_function = [0] * len(self.states)
             for state in self.states:
@@ -54,7 +56,7 @@ class MDP:
         # get policy
         self.policy = []
         for state in self.states:
-            max_value = 0
+            action_function = []
             for action in self._get_valid_actions(state):
                 if state-action <= 0:
                     lose_state = 0
@@ -70,15 +72,41 @@ class MDP:
                     win_reward = 0
                 reward = (1-self.p) * (lose_reward + self.gamma * lose_state) + \
                     self.p * (win_reward + self.gamma * win_state)
-                if reward > max_value:
-                    max_value = reward
-                    max_action = action
-            self.policy.append(max_action)
+                action_function.append([action, reward])
+                max_action_value = max(action_function, key=lambda x: x[1])[1]
+                max_actions = [action for action, reward in action_function \
+                               if reward == max_action_value]
+            self.policy.append(max_actions)
         print self.policy
-        plt.figure()
-        plt.plot(self.policy, '.--')
+        self._plot_value_function(value_functions)
+        self._plot_final_policy(self.policy)
+        
+    def _plot_value_function(self, value_functions):
+        show_index = [1, 2, 4, 20]
+        colors = ['#FF4040', '#6495ED', '#66CDAA', '#8470FF']
+        fig = plt.figure()
+        axs = []
+        for index, color in zip(show_index, colors):
+            axs.append(plt.plot(self.states, value_functions[index], '-', c=color))
+        plt.title('value function in different iterations')
+        plt.xlabel('states')
+        plt.ylabel('value function')
+        plt.legend([t[0] for t in axs], ['iter 1', 'iter 2', 'iter 4', 'iter 20'])
         plt.show()
-    
+        
+    def _plot_final_policy(self, policy):
+        state_actions = []
+        for state in range(len(policy)):
+            for action in policy[state]:
+                state_actions.append([state+1, action])
+        print state_actions
+        fig = plt.figure()
+        plt.plot([t[0] for t in state_actions], [t[1] for t in state_actions], '.', c='#6495ED')
+        plt.title('final target policy')
+        plt.xlabel('states')
+        plt.ylabel('action')
+        plt.show()
+            
     def _can_exit(self, last_value_function, value_function):
         can_exit = True
         for last, value in zip(last_value_function, value_function):
